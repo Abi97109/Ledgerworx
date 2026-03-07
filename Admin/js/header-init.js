74a094670c0c426df742602d3b987aeaa28cd489
Adithyan P S <adithyanps474@gmail.com>
2026-03-07 10:40:20 +0530

    Delete Admin directory

--- Admin/js/header-init.js
+++
@@ -1,8 +0,0 @@
-(function () {
-  try {
-    if (localStorage.getItem('ledger_dark') === '1') {
-      document.documentElement.classList.add('dark');
-      if (document.body) document.body.classList.add('dark');
-    }
-  } catch (e) {}
-})();
